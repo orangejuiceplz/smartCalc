568dc41bf21e7b931796f0d0fd8e67664c206401
Lamdashi <155986030+orangejuiceplz@users.noreply.github.com>
2024-05-29 07:54:45 -0400

    Update SmartCalculator.h

--- calc/SmartCalculator.h
+++ calc/SmartCalculator.h
@@ -29,8 +29,8 @@
 
 class SmartCalculator {
   public:
-    // Basic arithmetic operations
-    double add(double a, double b);
+  // Basic arithmetic operations
+  double add(double a, double b);
   double subtract(double a, double b);
   double multiply(double a, double b);
   double divide(double a, double b);
