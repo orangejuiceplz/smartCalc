c25e66da80c6b3adb40af425045a73fd0793d79c
Lamdashi <155986030+orangejuiceplz@users.noreply.github.com>
2024-05-30 09:12:28 -0400

    Update GraphingCalculator.h
    change from SFML to wxWidgets

--- calc/GraphingCalculator.h
+++ calc/GraphingCalculator.h
@@ -1,19 +1,23 @@
 #ifndef GRAPHINGCALCULATOR_H
 #define GRAPHINGCALCULATOR_H
 
-#include <SFML/Graphics.hpp>
+#include <wx/wx.h>
 #include <string>
 #include <vector>
 #include "SmartCalculator.h"
 
-class GraphingCalculator {
+class GraphingCalculator : public wxFrame {
 public:
     GraphingCalculator(SmartCalculator& calc);
-    void run();
+    void OnPaint(wxPaintEvent& event);
+    void OnClose(wxCloseEvent& event);
 
 private:
     double evaluateExpression(const std::string& expression, double x);
     SmartCalculator& calculator;
+    std::vector<std::string> functions;
+
+    wxDECLARE_EVENT_TABLE();
 };
 
 #endif
